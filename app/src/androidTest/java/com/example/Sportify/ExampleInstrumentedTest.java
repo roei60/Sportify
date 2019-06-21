1f0ac3c499a5f17bf28e191c050eedbdd5ed02ef
roei <roei@DESKTOP-QUB3G14>
2019-06-21 09:39:02 +0300

    add firebase support

--- app/src/androidTest/java/com/example/Sportify/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/Sportify/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.Sportify;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- app/src/test/java/com/example/Sportify/ExampleUnitTest.java
+++ app/src/test/java/com/example/Sportify/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.Sportify;
 
 import org.junit.Test;
 
