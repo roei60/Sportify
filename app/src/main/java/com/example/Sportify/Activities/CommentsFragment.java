7ea75db9a82d6404df6cb68e6782e7e7494fe13a
EtgarZ <etgarzyl92@gmail.com>
2019-08-05 23:21:31 +0300

    update comments fragment to enter new comment

--- app/src/main/java/com/example/Sportify/Activities/CommentsFragment.java
+++ app/src/main/java/com/example/Sportify/Activities/CommentsFragment.java
@@ -5,17 +5,26 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.Sportify.R;
+import com.example.Sportify.models.Comment;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CommentsFragment extends Fragment {
 
+    List<Comment> mComments;
+    ImageButton mSendBtn;
+    EditText mCommentText;
 
     public CommentsFragment() {
         // Required empty public constructor
@@ -26,7 +35,18 @@ public class CommentsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comments, container, false);
+        View view = inflater.inflate(R.layout.fragment_comments, container, false);
+
+        mSendBtn = view.findViewById(R.id.comments_send_btn);
+        mCommentText = view.findViewById(R.id.comments_edit_text);
+
+        mSendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Tag", "add comment clicked!");
+            }
+        });
+        return view;
     }
 
 }
