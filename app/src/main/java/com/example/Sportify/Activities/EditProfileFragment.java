11ba858968fb0566040f4d9464887ee279c50298
roei <roei@DESKTOP-QUB3G14>
2019-08-08 19:36:02 +0300

    fix layout of edit profile

--- app/src/main/java/com/example/Sportify/Activities/EditProfileFragment.java
+++ app/src/main/java/com/example/Sportify/Activities/EditProfileFragment.java
@@ -12,6 +12,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.Sportify.R;
+import com.example.Sportify.dal.Dao;
+import com.example.Sportify.models.User;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +23,7 @@ public class EditProfileFragment extends Fragment {
 
     ImageView editProfile_image;
     TextView editProfile_name_txt;
+    User currentUser;
     public EditProfileFragment() {
         // Required empty public constructor
     }
@@ -28,10 +32,14 @@ public class EditProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        currentUser= Dao.instance.getCurrentUser();
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
-      //  editProfile_name_txt=view.findViewById(R.id.)
-
+        editProfile_name_txt=view.findViewById(R.id.EditProfile_name_txt);
+        editProfile_image=view.findViewById(R.id.EditProfile_user_image_view);
+        editProfile_name_txt.setText(currentUser.getName());
+        Picasso.with(this.getContext()).load(currentUser.getImageUri()).fit().into(editProfile_image);
 
 
         return view;
