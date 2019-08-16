f4dd22f4d5400983cf4121c5d76fcc139d00a746
EtgarZ <etgarzyl92@gmail.com>
2019-08-16 23:52:43 +0300

    add spinner while getting post data + fix add and edit post

--- app/src/main/java/com/example/Sportify/Activities/PostFragment.java
+++ app/src/main/java/com/example/Sportify/Activities/PostFragment.java
@@ -1,6 +1,7 @@
 package com.example.Sportify.Activities;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.Sportify.R;
@@ -47,6 +49,12 @@ public class PostFragment extends Fragment {
     private Button mPostSendBt;
     PostViewModel viewModel;
     private Uri mPostImageUri;
+    private ProgressBar mProgressBar;
+    private ProgressDialog mProgressDialog;
+    private boolean isChangedPicture = false;
+
+    private static final String UPLOAD_MASSAGE_TEXT = "Uploading post..";
+
     public PostFragment() {
         // Required empty public constructor
     }
@@ -59,10 +67,12 @@ public class PostFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_post, container, false);
         mPostId =  CommentsFragmentArgs.fromBundle(getArguments()).getPostId();
 
+        mProgressDialog = new ProgressDialog(getActivity());
         mPostEditText = view.findViewById(R.id.post_edit_text);
         mPostImageView = view.findViewById(R.id.post_image_view);
         mAddPostImageBt = view.findViewById(R.id.post_add_picture_bt);
         mPostSendBt = view.findViewById(R.id.post_send_bt);
+        mProgressBar = view.findViewById(R.id.post_progressBar);
         viewModel= ViewModelProviders.of(this).get(PostViewModel.class);
         mAddPostImageBt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,6 +94,7 @@ public class PostFragment extends Fragment {
             });
         }
         else{ // new post
+            setLoadingUI(false);
             mPostSendBt.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -101,33 +112,14 @@ public class PostFragment extends Fragment {
 
                     // if post not have image just add post
                     if (mPostImageUri == null){
-                        viewModel.uploadPost(post, new Dao.AddPostListener() {
-                            @Override
-                            public void onComplete(Post post) {
-                            System.out.println("CreationDate : " + post.getCreationDate());
-                            Toast.makeText(getActivity(), "Post added successfully!", Toast.LENGTH_SHORT).show();
-                            Navigation.findNavController(getView()).navigate(R.id.action_postFragment_to_postsListFragment);
-                        }
-                        });
+                        mProgressDialog.setTitle(UPLOAD_MASSAGE_TEXT);
+                        mProgressDialog.show();
+                        addPost(post);
                     }
                     else{
-                        viewModel.uploadFile(mPostImageUri, new Dao.UploadFileListener() {
-                            @Override
-                            public void onComplete(Uri imageUri) {
-                                Toast.makeText(getActivity(), "Upload post image successfully!", Toast.LENGTH_SHORT).show();
-                                post.setPicture(imageUri.toString());
-                                viewModel.uploadPost(post, new Dao.AddPostListener() {
-                                    @Override
-                                    public void onComplete(Post post) {
-                                        Log.d("Tag", "Upload post successfully!");
-                                        System.out.println("CreationDate : " + post.getCreationDate());
-                                        Toast.makeText(getActivity(), "Post added successfully!", Toast.LENGTH_SHORT).show();
-                                        Navigation.findNavController(getView()).navigate(R.id.action_postFragment_to_postsListFragment);
-                                    }
-                                });
-
-                            }
-                        });
+                        mProgressDialog.setTitle(UPLOAD_MASSAGE_TEXT);
+                        mProgressDialog.show();
+                        uploadFileAndAddPost(mPostImageUri, post);
                     }
                 }
             });
@@ -136,10 +128,33 @@ public class PostFragment extends Fragment {
         return view;
     }
 
+    private void addPost(Post post){
+        viewModel.uploadPost(post, new Dao.AddPostListener() {
+            @Override
+            public void onComplete(Post post) {
+                System.out.println("CreationDate : " + post.getCreationDate());
+                mProgressDialog.hide();
+                Navigation.findNavController(getView()).navigate(R.id.action_postFragment_to_postsListFragment);
+                Toast.makeText(getActivity(), "Post added successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void uploadFileAndAddPost(Uri imageUri, Post post){
+        viewModel.uploadFile(imageUri, new Dao.UploadFileListener() {
+            @Override
+            public void onComplete(Uri imageUri) {
+                post.setPicture(imageUri.toString());
+                addPost(post);
+            }
+        });
+    }
+
     private void fillPostData(Post post){
 
+        setLoadingUI(true);
 
-            Log.d("Tag", "post.text = " + post.getText());
+        Log.d("Tag", "post.text = " + post.getText());
             mPostEditText.setText(post.getText());
 
             if (post.getPicture() != null){
@@ -155,34 +170,64 @@ public class PostFragment extends Fragment {
                     createPost(post);
                     // if post not have image just update post
                     if (mPostImageUri == null){
-                        viewModel.updatePost(post, new Dao.UpdatePostListener() {
-                            @Override
-                            public void onComplete(Post post) {
-                                System.out.println("CreationDate : " + post.getCreationDate());
-                                Toast.makeText(getActivity(), "Post updated successfully!", Toast.LENGTH_SHORT).show();
-                                Navigation.findNavController(getView()).navigate(R.id.action_postFragment_to_postsListFragment);
-                            }
-                        });
+                        mProgressDialog.setTitle(UPLOAD_MASSAGE_TEXT);
+                        mProgressDialog.show();
+                        updatePost(post);
                         return;
                     }
                     // upload post image and then add post with the image url
-                    viewModel.uploadFile(mPostImageUri, new Dao.UploadFileListener() {
-                        @Override
-                        public void onComplete(Uri imageUri) {
-                            Toast.makeText(getActivity(), "Upload post image successfully!", Toast.LENGTH_SHORT).show();
-                            post.setPicture(imageUri.toString());
-                            viewModel.updatePost(post, new Dao.UpdatePostListener() {
-                                @Override
-                                public void onComplete(Post post) {
-                                    System.out.println("CreationDate : " + post.getCreationDate());
-                                    Toast.makeText(getActivity(), "Post updated successfully!", Toast.LENGTH_SHORT).show();
-                                    Navigation.findNavController(getView()).navigate(R.id.action_postFragment_to_postsListFragment);
-                                }
-                            });
-                        }
-                    });
+                    mProgressDialog.setTitle(UPLOAD_MASSAGE_TEXT);
+                    mProgressDialog.show();
+                    if (isChangedPicture){
+                        uploadFileAndUpdatePost(mPostImageUri, post);
+                    }
+                    else{
+                        updatePost(post);
+                    }
                 }
             });
+
+        setLoadingUI(false);
+    }
+
+    private void uploadFileAndUpdatePost(Uri imageUri, Post post){
+        viewModel.uploadFile(imageUri, new Dao.UploadFileListener() {
+            @Override
+            public void onComplete(Uri imageUri) {
+                post.setPicture(imageUri.toString());
+                updatePost(post);
+            }
+        });
+    }
+
+    private void updatePost(Post post){
+        viewModel.updatePost(post, new Dao.UpdatePostListener() {
+            @Override
+            public void onComplete(Post post) {
+                System.out.println("CreationDate : " + post.getCreationDate());
+                mProgressDialog.hide();
+                Navigation.findNavController(getView()).navigate(R.id.action_postFragment_to_postsListFragment);
+                Toast.makeText(getActivity(), "Post updated successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setLoadingUI(boolean isLoading){
+        if (isLoading){
+            mProgressBar.setVisibility(View.VISIBLE);
+            mPostEditText.setVisibility(View.INVISIBLE);
+            mPostSendBt.setVisibility(View.INVISIBLE);
+            mPostImageView.setVisibility(View.INVISIBLE);
+            mAddPostImageBt.hide();
+        }
+        else{
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mPostEditText.setVisibility(View.VISIBLE);
+            mPostSendBt.setVisibility(View.VISIBLE);
+            mPostImageView.setVisibility(View.VISIBLE);
+            mAddPostImageBt.show();
+        }
+
     }
 
     private Post createPost(Post post){
@@ -202,8 +247,10 @@ public class PostFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_OK)
+        if (resultCode != RESULT_OK){
+            mPostImageUri = null;
             return;
+        }
 
         try {
             // handle result of CropImageActivity
@@ -211,6 +258,7 @@ public class PostFragment extends Fragment {
             {
                 CropImage.ActivityResult result = CropImage.getActivityResult(data);
                 if (resultCode == RESULT_OK) {
+                    isChangedPicture = true;
                     mPostImageUri = result.getUri();
                     Picasso.with(this.getContext()).load(result.getUri()).fit().into(mPostImageView);
                 } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
